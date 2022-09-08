8b4d334b91388ec505118075b660ef397c7abf8a
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-09-08 10:22:20 -0400

    new line fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,9 @@
  * main - Entery point
  *
  * Return: 1
- *
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
