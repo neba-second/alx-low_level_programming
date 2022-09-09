cce0e4f728d64f71f7bea1aa8c39e185205fa644
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-09-09 12:25:59 -0400

    printing all possible combination of three digit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		int j = i + 1;
+
+		while (j <= 57)
+		{
+			int k = j + 1;
+
+			while (k <= 57)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 56 || j != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
