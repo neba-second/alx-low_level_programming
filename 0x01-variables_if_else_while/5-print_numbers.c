ad1d8f8ae2c4b8eef759564a62e390158448a9fa
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-09-09 10:09:34 -0400

    printing from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
