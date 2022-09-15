0dd35b2dcafd25622d1a52e2258f66269a14fb73
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-09-15 05:05:38 -0400

    ADD 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of jack Bauer
+ *
+ * Return: no return
+ *
+ */
+void jack_bauer(void)
+{
+	int mt, mo, st, so;
+
+	for (mt = 0; mt <= 2; mt++)
+	{
+		for (mo = 0; mo <= 3; mo++)
+		{
+			for (st = 0; st <= 5; st++)
+			{
+				for (so = 0; so <= 9; so++)
+				{
+					_putchar(mt + '0');
+					_putchar(mo + '0');
+					_putchar(':');
+					_putchar(st + '0');
+					_putchar(so + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
