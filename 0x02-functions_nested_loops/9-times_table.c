fa219f1de469a71995ceb26a97c5c23517f56333
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-09-15 07:33:56 -0400

    ADD 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - Prints 9 times table
+ *
+ * Return: no return
+ *
+ */
+void times_table(void)
+{
+	int i, j, result, o, t;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = j * i;
+			if (result > 9)
+			{
+				o = result % 10;
+				t = result / 10;
+				_putchar(t + '0');
+				_putchar(o + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
