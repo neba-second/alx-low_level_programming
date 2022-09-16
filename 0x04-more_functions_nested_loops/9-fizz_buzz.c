25458a3803663fc5646cb866b18949b0ba1f58e0
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-09-16 13:23:52 -0400

    ADD 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1-100
+ * Description: prints fizz if multiply 3 buzz if 5 and fizzbuzz if both or the number
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	return (0);
+}
