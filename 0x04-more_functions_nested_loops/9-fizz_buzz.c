b20987545a5e8d50f2d12eb5e4a3225b528832fb
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-09-16 13:26:30 -0400

    fixing betty

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints 1-100
- * Description: prints fizz if multiply 3 buzz if 5 and fizzbuzz if both or the number
+ * Description: prints fizz if multiply 3 buzz if 5 and fizzbuzz for both
  *
  * Return: always 0
  */
@@ -13,21 +13,13 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("FizzBuzz");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz");
-		}
 		else
-		{
 			printf("%d", i);
-		}
 		printf(" ");
 	}
 	return (0);
