c4e7234cecbc362e9e51739d00105b020df54841
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-09-16 07:53:10 -0400

    ADD gcc 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0-9 followed by a new line
+ *
+ * Return: no return
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
