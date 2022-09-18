083b9f547ca7b6c0123f6d28a3bcaeabff388ddd
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-09-18 10:36:59 -0400

    fixing output on 11-printing natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all natural numbers to 98
+ * @n: starting point for the program
+ *
+ * Return: no return
+ *
+ */
+void print_to_98(int n)
+{
+	int i = n;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+				printf(" %d,", i);
+			}
+			else
+			{
+				printf(" %d", i);
+			}
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf(" %d,", i);
+			}
+			else
+			{
+				printf(" %d", i);
+			}
+		}
+		printf("\n");
+	}
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,31 +8,37 @@
  */
 void times_table(void)
 {
-	int i, j, result, o, t;
+	int i, j, k, o, t;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (j == 0 && i == 0)
+			k = i * j;
+			if (j == 9)
+                        {
+                                _putchar(' ');
+                                _putchar(k + '0');
+                        }
+			else if (j != 0)
 			{
-				_putchar('0');
-			}
-			result = j * i;
-			if (result > 9)
-			{
-				o = result % 10;
-				t = result / 10;
 				_putchar(' ');
-				_putchar(t + '0');
-				_putchar(o + '0');
+				_putchar(' ');
+				_putchar(k + '0');
 				_putchar(',');
 			}
-			else if (result < 10)
+			else if (k > 9)
+                        {
+                                t = k / 10;
+                                o = k % 10;
+                                _putchar(' ');
+                                _putchar(t + '0');
+                                _putchar(o + '0');
+                                _putchar(',');
+                        }
+			else
 			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(result + '0');
+				_putchar(k + '0');
 				_putchar(',');
 			}
 		}
