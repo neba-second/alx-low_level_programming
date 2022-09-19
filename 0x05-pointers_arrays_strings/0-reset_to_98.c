dd4721bb647959345ac4cd9f3f9bbc90dcada003
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-09-19 09:16:40 -0400

    ADD 0-reset_to_98.c, main.h, _putchar.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes the variable value to 98
+ * @n: the variable pointer used to change the value
+ *
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
