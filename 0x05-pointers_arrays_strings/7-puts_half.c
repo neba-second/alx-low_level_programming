bab24ac049791ce4c7eaee8c41095c5a807336e2
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-09-22 08:02:56 -0400

    ADD 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the length of the string
+ * @s: the pointer to the string
+ *
+ * Return: int the length of the string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s)
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+
+/**
+ * puts_half - prints the second half of the string
+ * @str: the pointer to the string
+ */
+void puts_half(char *str)
+{
+	int size = _strlen(str);
+	int i, j;
+
+	if (size % 2 == 0)
+	{
+		i = size / 2;
+	}
+	else
+	{
+		i = (size - 1) / 2;
+	}
+	for (j = i; j < i * 2; j++)
+	{
+		_putchar(*(str + j));
+	}
+	_putchar('\n');
+}
