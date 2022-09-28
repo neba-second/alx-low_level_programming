c1943efec7eaba07bf3859ee832c7b5c3d703ef6
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-09-28 10:12:04 -0400

    1-print_rev_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates the length of a string using recursion
+ * @s: the pointer to the string
+ *
+ * Return: int the length
+ */
+int _strlen_recursion(char *s)
+{
+
+}
