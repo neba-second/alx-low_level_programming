b037d23b3c602fd1b85f4599025fb34f5925b125
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-10-04 16:20:58 -0400

    ADD 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array
+ * @width:  the elements in each array
+ * @height: the amount of arrays to be nested
+ *
+ * Return: pointer to the array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i, j;
+
+	if (height <= 0 || width <= 0)
+		return (NULL);
+	ptr = (int **)malloc(sizeof(int) * height);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = (int *)malloc(sizeof(int) * width);
+		if (ptr[i] == NULL)
+			return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
+}
