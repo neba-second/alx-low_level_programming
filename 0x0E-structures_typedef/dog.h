513691610d4a0ca5cc6cac2b12b89c2e9fab23a5
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-10-11 10:56:54 -0400

    ADD 4-new_dog.c, creates a new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * struct dog - describes dog
  * @name: the name
@@ -17,4 +18,6 @@ struct dog
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
