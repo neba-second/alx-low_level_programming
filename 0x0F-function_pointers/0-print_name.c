2cbfab26992c38aad5fa947e435785062c2e7a8f
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-10-12 16:28:18 -0400

    ADD 0-print_name.c, prints name using function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - func that prints a name
+ * @name: the name to print
+ * @f: function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
