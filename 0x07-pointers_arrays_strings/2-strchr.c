2f1edc5ecca4aa7a8f5056b7e1c6711e5ceb30b2
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-10-16 12:24:11 -0400

    returning for null

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,7 +13,7 @@ char *_strchr(char *s, char c)
 	{
 	char **ptr = &s;
 
-	while (*s + 1)
+	while (*s)
 	{
 		if (*s == c)
 		{
@@ -21,8 +21,6 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	return (*ptr);
 	}
-	else
 	return (NULL);
 }
