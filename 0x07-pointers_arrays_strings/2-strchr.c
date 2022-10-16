09857ad28ae525fb45ceb32cc51d706e3f29d546
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-10-16 12:28:35 -0400

    fixing null problem

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,26 +1,21 @@
 #include "main.h"
-#include <stddef.h>
+
 /**
- * _strchr - locates a character in a string
- * @s: the pointer to the string
- * @c: the character to look for
+ * _strchr - locates a char in a string
+ * @s: string to be searched
+ * @c: char to be checked
  *
- * Return: a pointer to the first occurrence of c
+ * Return: pointer to the first occurence of c in s
  */
 char *_strchr(char *s, char c)
 {
-	if (s)
-	{
-	char **ptr = &s;
+	int i = 0;
 
-	while (*s)
-	{
-		if (*s == c)
-		{
-			return (*ptr);
-		}
-		s++;
-	}
-	}
-	return (NULL);
+	for (; s[i] != c && s[i] != '\0'; i++)
+		;
+
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
 }
