a4b7d82694446a5a3c1ea6cbd0fcda3d2a02cc41
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-10-20 13:19:03 -0400

    edit lists.h, double inclusion and space fixed on header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,19 +1,19 @@
-#ifndef LISTS_h
+#ifndef LISTS_H
 #define LISTS_H
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
+* struct list_s - singly linked list
+* @str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+*/
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 #include <stdio.h>
 int _putchar(char c);
