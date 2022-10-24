cc9010e3b665dbc922dec2ed58be6c21e7a17986
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-10-24 13:42:48 -0400

    fix on task 5 making the head null try 4

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,16 +7,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp = *head;
+	listint_t *tmp;
 
 	if (*head != NULL)
 	{
-		while (tmp != NULL)
+		while (*head != NULL)
 		{
-			tmp = tmp->next;
-			free(*head);
-			*head = tmp;
+			tmp = *head;
+			*head = tmp->next;
+			free(tmp);
 		}
-		free(*head);
 	}
 }
