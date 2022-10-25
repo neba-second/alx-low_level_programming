14b919ce95e262c92b48b2a24cd5d0f42428dacf
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-10-25 11:38:25 -0400

    ADD 9-insert_nodeint.c, inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,23 @@
 #include "lists.h"
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ *
+ * @h: head ponter
+ *
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+        const listint_t *tmp = h;
+        int count = 0;
+
+        while (tmp != 0)
+        {
+                tmp = tmp->next;
+                count++;
+        }
+        return (count);
+}
 
 /**
  * insert_nodeint_at_index - inserts a new node at a given index
@@ -12,8 +31,12 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr, *new_node;
+	unsigned int size;
 
 	ptr = *head;
+	size = listint_len(ptr);
+	if (idx < size)
+	{
 	while (idx > 1)
 	{
 		if (ptr != NULL)
@@ -29,6 +52,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_node->next = ptr->next;
 		ptr->next = new_node;
 		return (new_node);
+	}
+	else
+		return (NULL);
 	}
 	else
 		return (NULL);
