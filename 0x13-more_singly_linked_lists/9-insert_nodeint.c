626419e26f63ef72de0a0f5b872c30bc67e1d136
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-10-25 11:42:24 -0400

    recode to fix most of the problems

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,62 +1,43 @@
 #include "lists.h"
 /**
- * listint_len - returns the number of elements in a linked listint_t list
- *
- * @h: head ponter
- *
- * Return: number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-        const listint_t *tmp = h;
-        int count = 0;
-
-        while (tmp != 0)
-        {
-                tmp = tmp->next;
-                count++;
-        }
-        return (count);
-}
-
-/**
- * insert_nodeint_at_index - inserts a new node at a given index
- *
- * @head: pointer to pointer of first node
- * @idx: the index to insert the node
- * @n: the data to insert in the new node
- *
- * Return: pointer to the new node
- */
+  * insert_nodeint_at_index - inserts a new node at a given position.
+  * @head: pointer to pointer of the first element on list.
+  * @idx: index of list where the new node should be added.
+  * @n: integer to be inserted.
+  *
+  * Return: address of the new node or NULL if it fails.
+  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *ptr, *new_node;
-	unsigned int size;
+	listint_t *new_node, *temp;
+	unsigned int count;
 
-	ptr = *head;
-	size = listint_len(ptr);
-	if (idx < size - 1)
+	temp = *head;
+
+	count = 0;
+	while (temp && count < idx - 1)
 	{
-	while (idx > 1)
-	{
-		if (ptr != NULL)
-			ptr = ptr->next;
-		else
-			return (NULL);
-		idx--;
+		temp = temp->next;
+		count++;
 	}
-	new_node = (listint_t *)malloc(sizeof(listint_t));
+
+	new_node = malloc(sizeof(listint_t));
 	if (new_node != NULL)
 	{
 		new_node->n = n;
-		new_node->next = ptr->next;
-		ptr->next = new_node;
-		return (new_node);
+		if (idx == 0)
+		{
+			new_node->next = *head;
+			*head = new_node;
+			return (new_node);
+		}
+		if (count + 1 == idx)
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+			return (new_node);
+		}
 	}
-	else
-		return (NULL);
-	}
-	else
-		return (NULL);
+	free(new_node);
+	return (NULL);
 }
-
