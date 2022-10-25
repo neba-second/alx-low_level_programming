431bbbc52ccd91106c01019b79055de83b90e7e2
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-10-25 11:40:02 -0400

    some fixes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	ptr = *head;
 	size = listint_len(ptr);
-	if (idx < size)
+	if (idx < size - 1)
 	{
 	while (idx > 1)
 	{
