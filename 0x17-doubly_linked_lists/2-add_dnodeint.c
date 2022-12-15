c3f9b660088afa0126388ba91c04c5fe06ac9be5
Nebil Abdulfetah <nebilabdulfetah33@gmail.com>
2022-12-15 11:09:52 -0500

    rewrite on 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,29 +1,38 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds new node at the beginning of a dlistint_t
+ * add_dnodeint - adds a new node at the beginning
+ * of a dlistint_t list
  *
- * @head: pointer to pointer of head
- * @n: the element to add
- *
- * Return: the address of the new element or NULL if it failed
- *
+ * @head: head of the list
+ * @n: value of the element
+ * Return: the address of the new element
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
+	dlistint_t *new;
+	dlistint_t *h;
 
-	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
-	if (new_node)
-	{
-		new_node->n = n;
-		new_node->prev = NULL;
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
-	else
-	{
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
 	}
+
+	new->next = h;
+
+	if (h != NULL)
+		h->prev = new;
+
+	*head = new;
+
+	return (new);
 }
